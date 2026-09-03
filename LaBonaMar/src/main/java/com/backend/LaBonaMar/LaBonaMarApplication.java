c5be94247775ebbaa29e4bb74c488f9b882b0b71
Anelef14 <anelef14@gmail.com>
2026-09-03 11:14:50 +0200

    Initial Spring Boot project with Maven wrapper
    Add initial Spring Boot application skeleton: pom.xml (Spring Boot 4.1.1, Java 17) with dependencies for JPA, WebMVC, MySQL connector and Lombok; include Maven Wrapper (.mvn, mvnw, mvnw.cmd and maven-wrapper.properties). Also add .gitignore and .gitattributes, main application class, application.properties, and a basic context-load test.

---
+++ LaBonaMar/src/main/java/com/backend/LaBonaMar/LaBonaMarApplication.java
@@ -0,0 +1,13 @@
+package com.backend.LaBonaMar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaBonaMarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaBonaMarApplication.class, args);
+	}
+
+}
---
+++ LaBonaMar/src/test/java/com/backend/LaBonaMar/LaBonaMarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.LaBonaMar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaBonaMarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
